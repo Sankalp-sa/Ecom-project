f9ab5c106bdcb316d8c4453046f99e2f52a404c0
Sankalp-sa <sankalp.p@ahduni.edu.in>
2023-06-13 18:41:28 +0530

    Created Category api

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 
 import connectDB from './config/db.js';
 import authRoute from './routes/authRoute.js';
+import categoryRoute from './routes/categoryRoute.js';
 
 // set cors to *
 
@@ -22,6 +23,7 @@ app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoute);
 
 const port = process.env.PORT || 8080;
 
